4419bd72f290b83572b0b869d21d020f643f570b
jungtaek kim <kgajungtaek@gmail.com>
2022-12-27 15:14:01 +0900

    feat : 범위 공격의 각도 적용

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -115,11 +115,20 @@ public class PlayerController : MonoBehaviour
 	{
 		Debug.Log("공격 타이밍");
 
+		// 1. 범위내에 있는가
 		Collider[] colliders = Physics.OverlapSphere(transform.position, attackRange);
 		for(int i = 0; i < colliders.Length; i++)
 		{
-			if (colliders[i].gameObject.name == "Cube")
-				Destroy(colliders[i].gameObject);
+			if (colliders[i].gameObject.name != "Cube")
+				continue;
+
+			Vector3 dirToTarget = (colliders[i].transform.position - transform.position).normalized;
+
+			// 2. 각도내에 있는가
+			if (Vector3.Dot(transform.forward, dirToTarget) < Mathf.Cos(attackAngle * 0.5f * Mathf.Deg2Rad))
+				continue;
+
+			Destroy(colliders[i].gameObject);
 		}
 	}
 
