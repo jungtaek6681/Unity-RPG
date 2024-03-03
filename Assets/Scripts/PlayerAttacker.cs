b7b6b1f2fbc3dce5504f05ea7457b18585c91412
jungtaek kim <kgajungtaek@gmail.com>
2024-03-03 20:53:34 +0900

    feat : 백터의 내적을 이용한 범위공격구현
    백터의 내적을 이용하여 각도계산을 빠르게 계산할 수 있으며, 이를 응용하여 부담되는 삼각함수 대신 내적으로 최적화

--- Assets/Scripts/PlayerAttacker.cs
+++ Assets/Scripts/PlayerAttacker.cs
@@ -4,16 +4,39 @@ using UnityEngine.InputSystem;
 public class PlayerAttacker : MonoBehaviour
 {
     [SerializeField] Animator animator;
-    [SerializeField] Weapon weapon;
+    [SerializeField] LayerMask layerMask;
+    [SerializeField] float range;
+    [SerializeField, Range(0, 360)] float angle;
+    [SerializeField] int damage;
 
-    private void StartAttack()
+    private float preAngle;
+    private float cosAngle;
+    private float CosAngle
     {
-        weapon.EnableAttack();
+        get
+        {
+            if (preAngle == angle)
+                return cosAngle;
+
+            preAngle = angle;
+            cosAngle = Mathf.Cos(angle * 0.5f * Mathf.Deg2Rad);
+            return cosAngle;
+        }
     }
 
-    private void EndAttack()
+    Collider[] colliders = new Collider[20];
+    private void AttackTiming()
     {
-        weapon.DisableAttack();
+        int size = Physics.OverlapSphereNonAlloc(transform.position, range, colliders, layerMask);
+        for (int i = 0; i < size; i++)
+        {
+            Vector3 dirToTarget = (colliders[i].transform.position - transform.position).normalized;
+            if (Vector3.Dot(transform.forward, dirToTarget) < CosAngle)
+                continue;
+
+            IDamagable damagable = colliders[i].GetComponent<IDamagable>();
+            damagable?.TakeDamage(damage);
+        }
     }
 
     private void Attack()
@@ -27,5 +50,17 @@ public class PlayerAttacker : MonoBehaviour
         {
             Attack();
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+
+        Vector3 rightDir = Quaternion.Euler(0, angle * 0.5f, 0) * transform.forward;
+        Vector3 leftDir = Quaternion.Euler(0, angle * -0.5f, 0) * transform.forward;
+
+        Debug.DrawRay(transform.position, rightDir * range, Color.cyan);
+        Debug.DrawRay(transform.position, leftDir * range, Color.cyan);
     }
 }
