e7e0cecf61087f324ef01be72616664a8b70fe1e
jungtaek kim <kgajungtaek@gmail.com>
2024-03-04 02:20:28 +0900

    feat : C# 파일 입출력
    파일의 경로를 지정한 뒤 저장 & 불러오기를 진행

--- Assets/Scripts/DataController.cs
+++ Assets/Scripts/DataController.cs
@@ -1,16 +1,48 @@
+using System.IO;
 using UnityEngine;
 
 public class DataController : MonoBehaviour
 {
+    public int value;
+
+    private void Start()
+    {
+        Load();
+    }
+
     [ContextMenu("Save")]
     public void Save()
     {
-        Debug.Log("Save");
+#if UNITY_EDITOR
+        string path = Path.Combine(Application.dataPath, "Data.txt");
+#else
+        string path = Path.Combine(Application.persistentDataPath, "data.text");
+#endif
+        File.WriteAllText(path, value.ToString());
     }
 
     [ContextMenu("Load")]
     public void Load()
     {
-        Debug.Log("Load");
+#if UNITY_EDITOR
+        string path = Path.Combine(Application.dataPath, "Data.txt");
+#else
+        string path = Path.Combine(Application.persistentDataPath, "data.text");
+#endif
+        if (File.Exists(path) == false)
+        {
+            Create();
+        }
+        else
+        {
+            string text = File.ReadAllText(path);
+            value = int.Parse(text);
+        }
+    }
+
+    private void Create()
+    {
+        value = 0;
+        Save();
     }
 }
